8f6f285faaa243bd3e855bc39d6e954602424a63
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-18 13:59:02 -0700

    Update mcpelauncher_api.h

--- include/mcpelauncher_api.h
+++ include/mcpelauncher_api.h
@@ -4,21 +4,21 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+    #endif
 
-enum mcpelauncher_log_level {
-    MCPELAUNCHER_LOG_TRACE,
-    MCPELAUNCHER_LOG_DEBUG,
-    MCPELAUNCHER_LOG_INFO,
-    MCPELAUNCHER_LOG_WARN,
-    MCPELAUNCHER_LOG_ERROR
-};
+    enum mcpelauncher_log_level {
+        MCPELAUNCHER_LOG_TRACE,
+        MCPELAUNCHER_LOG_DEBUG,
+        MCPELAUNCHER_LOG_INFO,
+        MCPELAUNCHER_LOG_WARN,
+        MCPELAUNCHER_LOG_ERROR
+    };
 
-void *mcpelauncher_hook(void *symbol, void *hook, void **original);
+    void *mcpelauncher_hook(void *symbol, void *hook, void **original);
 
-void mcpelauncher_vlog(mcpelauncher_log_level level, const char *tag, const char *text, va_list args);
-void mcpelauncher_log(mcpelauncher_log_level level, const char *tag, const char *text, ...);
+    void mcpelauncher_vlog(mcpelauncher_log_level level, const char *tag, const char *text, va_list args);
+    void mcpelauncher_log(mcpelauncher_log_level level, const char *tag, const char *text, ...);
 
-#ifdef __cplusplus
+    #ifdef __cplusplus
 }
 #endif
